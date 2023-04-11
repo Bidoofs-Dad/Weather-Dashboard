56c1a5a8657970c06fef8e774ea7233b574f97ad
Zane Werle <zanewerle@gmail.com>
2023-04-11 14:22:57 -0500

    added in code that saves the city name inside of the Local Stroage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,23 @@
-var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=0ed7ee275eef2d8b5bd098a35449f8a4"
-var getCoordinates = "https://api.openweathermap.org/data/2.5/forecast?q=" + city +"&appid=0ed7ee275eef2d8b5bd098a35449f8a4"
-var city = "Minneapolis"
+var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=0ed7ee275eef2d8b5bd098a35449f8a4";
+var city = document.querySelector("#citySearch");
+var searchBtnEl = document.querySelector("#searchBtn");
+var getCoordinates = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=0ed7ee275eef2d8b5bd098a35449f8a4";
 
-function citySearch(getCoordinates){
-    fetch(getCoordinates)
-    .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data);
-      });
+function citySearch(){
+  searchBtnEl.addEventListener("click", function(){
+    var cityValue = city.value;
+    console.log(cityValue);
+    localStorage.setItem(cityValue, cityValue)
+  })
 }
+citySearch()
 
-// function getApi(requestUrl) {
-//     fetch(requestUrl)
-//       .then(function (response) {
-//         console.log(response);
-//         if (response.status === 200) {
-//           responseText.textContent = response.status;
-//         }
-//         return response.json();
-//     });
-//   }
+
+// fetch(getCoordinates)
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     console.log(data);
+//   });
+
