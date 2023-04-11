c61f00daea02551c0c1d06caf0fc409faecb7eca
Zane Werle <zanewerle@gmail.com>
2023-04-11 14:49:00 -0500

    added in the code that makes it so once you click the search button, it creates new buttons below it with the data from the localStorage. Still haven't tied it into the API yet though.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,16 +2,42 @@ var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon
 var city = document.querySelector("#citySearch");
 var searchBtnEl = document.querySelector("#searchBtn");
 var getCoordinates = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=0ed7ee275eef2d8b5bd098a35449f8a4";
+var cityList = document.querySelector("#cityList");
 
-function citySearch(){
-  searchBtnEl.addEventListener("click", function(){
+
+function displayCities() {
+  const citiesList = document.querySelector(".list-group");
+  citiesList.innerHTML = "";
+}
+
+function citySearch() {
+  searchBtnEl.addEventListener("click", function () {
     var cityValue = city.value;
     console.log(cityValue);
-    localStorage.setItem(cityValue, cityValue)
-  })
+    localStorage.setItem(cityValue, cityValue);
+    city.value = "";
+
+    var newCityBtn = document.createElement("button");
+    newCityBtn.setAttribute("class", "list-group-item list-group-item-action");
+    newCityBtn.setAttribute("type", "button");
+    newCityBtn.textContent = cityValue;
+    cityList.appendChild(newCityBtn);
+  });
+
+  for (var i = 0; i < localStorage.length; i++) {
+    var key = localStorage.key(i);
+    if (key !== "test") {
+      var newCityBtn = document.createElement("button");
+      newCityBtn.setAttribute("class", "list-group-item list-group-item-action");
+      newCityBtn.setAttribute("type", "button");
+      newCityBtn.textContent = localStorage.getItem(key);
+      cityList.appendChild(newCityBtn);
+    }
+  }
 }
 citySearch()
 
+//<!-- <a href="#" class="list-group-item list-group-item-action"></a> -->
 
 // fetch(getCoordinates)
 //   .then(function (response) {
